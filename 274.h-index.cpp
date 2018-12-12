711fcc83ff752584141112ec49ed2d78cc300e9e
banixc <banixc@gmail.com>
2018-12-12 21:31:07 +0800

    add 274

---
+++ 274.h-index.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+	int hIndex(vector<int>& citations) {
+		sort(citations.begin(), citations.end());
+		for(int i = 0; i < citations.size(); i ++) {
+			int h = citations.size()-i;
+			if(citations[i]>=h){
+				return h;
+			}
+		}
+		return 0;
+	}
+};
